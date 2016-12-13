bfda60b55a08f074860fd63adb8b5251eda380f9
Safwan Kamarrudin <shaihulud@alumni.cmu.edu>
2016-12-13 23:23:01 +1100

    Fix syntax error

--- src/include/header1.hrl
+++ src/include/header1.hrl
@@ -2,7 +2,7 @@
 {
     normal_field :: integer(),
     nested_field_same_file = #child_record_same_file{} :: child_record_same_file(),
-    nested_field_diff_file :: #child_record_diff_file{} :: child_record_diff_file()
+    nested_field_diff_file = #child_record_diff_file{} :: child_record_diff_file()
 }).
 
 -record(child_record_same_file,
